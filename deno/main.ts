0ff0ff59cb6dbdbb6a6c0a1f31e87bfc52b850a5
laguaer <129600040+laguaer@users.noreply.github.com>
2023-06-14 12:32:01 +0800

    Update main.ts

--- deno/main.ts
+++ deno/main.ts
@@ -4,17 +4,15 @@ serve(async (request) => {
       const url = new URL(request.url);
       const hostname = url.hostname;
       url.host = "github.com";
-      const req = new Request(url, request);
-      req.headers.set('origin', 'https://github.com');
+  
+      request.headers.set('origin', 'https://github.com');
       
-      const res = await fetch(req);
-      let newRes = new Response(res.body, res);
+      let newRes = await fetch(url, request)
       
       let location = newRes.headers.get('location');
       if (location !== null && location !== "") {
-            location = location.replace('://github.com', '://freegit.deno.dev');
+            location = location.replace('://github.com', '://' + hostname);
             newRes.headers.set('location', location);
       }
       return newRes 
 });
-
