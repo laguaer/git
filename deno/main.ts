24e024887904a11cf2c236b4725f8eb5e527ce49
laguaer <129600040+laguaer@users.noreply.github.com>
2023-06-14 11:12:41 +0800

    Update main.ts

--- deno/main.ts
+++ deno/main.ts
@@ -2,6 +2,19 @@ import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 
 serve(async (request) => {
     const url = new URL(request.url);
+    const hostname = url.hostname;
     url.host = "github.com";
-    return await fetch(url, request);
+    const req = new Request(url, request);
+    req.headers.set('origin', 'https://github.com');
+    
+    const res = await fetch(req);
+    let newRes = new Response(res.body, res);
+    
+    let location = newRes.headers.get('location');
+    if (location !== null && location !== "") {
+        location = location.replace('://gihub.com', '://' + hostname);
+        newRes.headers.set('location', location);
+    }
+    return newRes
 });
+
